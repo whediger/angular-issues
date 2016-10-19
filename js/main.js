78928296e3a461a702924941ea50e0a40bdcd32e
Wes Hediger <wes.hediger@gmail.com>
2016-10-19 16:30:44 -0600

    add remaining functionality

--- js/main.js
+++ js/main.js
@@ -11,36 +11,48 @@ function getData(urlIn){
   httpGetAsync(urlIn, function(data){
     data = JSON.parse(data);
     filterResults(data);
-    console.log(data);
   });
 }
 
+
 function filterResults(data){
-    var issueIndex = Angular.issues.length;
-    if(Angular.issues.length > 0)
-      issueIndex -= 1;
+
+  var ifIn7days = true;
+  var issueIndex = Angular.issues.length;
+  if(Angular.issues.length > 0)
+    issueIndex -= 1;
 
   for(i in data){
-      Angular.issues[issueIndex] = {
-        title: data[i].title,
-        body: data[i].body,
-        user: data[i].user.login
-      }
-      issueIndex++;
-      var d = new Date(data[i].created_at)
-      var today = new Date();
-      today = today.getTime();
-      //7 days = 604800000
-      if (Math.abs(today - d) < 43200000) {
-        console.log("less than half day");
-      } else {
-        console.log("more than half day");
-      }
+    //check if post is over 7 days old
+    var d = new Date(data[i].created_at)
+    var today = new Date();
+    today = today.getTime();
+    //.5 day = 43200000
+    if (Math.abs(today - d) < 604800000) {
+    } else {
+      ifIn7days = false;
+      break;
+    }
+
+    //convert mardown to html
+    markdownConverter = new showdown.Converter();
+    data[i].body = markdownConverter.makeHtml(data[i].body);
+
+    Angular.issues[issueIndex] = {
+      title: data[i].title,
+      body: data[i].body,
+      user: data[i].user.login,
+      assignee: data[i].assignee
+    }
+    issueIndex++;
   }
-  //if less than 7 days page++
-  //getData('https://api.github.com/repos/angular/angular/issues?page='+page);
-  //if more call add div function
-  console.log(Angular.issues);
+  //if less than 7 days page++ and get more;
+  if (ifIn7days){
+    page++
+    getData('https://api.github.com/repos/angular/angular/issues?page='+page);
+  } else {
+    return;
+  }
   addIssueDivs(Angular.issues)
 }
 
@@ -49,9 +61,27 @@ function addIssueDivs(issues){
   for (var i in issues){
     var issueDiv = document.createElement('div');
     var user = document.createElement('h3');
-    user.innerHTML = "user: " + issues[i].user;
-    user.className = "issueDiv";
+    var assignee = document.createElement('h3');
+    var title = document.createElement('h2');
+    var line = document.createElement('hr');
+    var body = document.createElement('p');
+
+    issueDiv.className = "issueDiv";
+    user.innerHTML = "<span class='issueKey'>user:</span> " + issues[i].user;
+    if (issues[i].assignee == null){
+      assignee.innerHTML = "<span class='issueKey'>assignee: </span>none";
+    } else {
+      assignee.innerHTML = "<span class='issueKey'>assignee: </span>" + issues[i].assignee;
+    }
+    title.innerHTML = "<span class='issueKey'>title:</span> " + issues[i].title;
+    body.innerHTML = issues[i].body;
+
+    //append childern +==}========>
     issueDiv.appendChild(user);
+    issueDiv.appendChild(assignee);
+    issueDiv.appendChild(title);
+    issueDiv.appendChild(line);
+    issueDiv.appendChild(body);
     issueContainer.appendChild(issueDiv);
   }
 
