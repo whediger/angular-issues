9eda0bfc29919aea0bc8f397e84749c633e5eecd
Wes Hediger <wes.hediger@gmail.com>
2016-10-19 11:38:43 -0600

    add api call for first page of data

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+httpGetAsync('https://api.github.com/repos/angular/angular/issues', function(data){
+  console.log(data);
+});
+
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
